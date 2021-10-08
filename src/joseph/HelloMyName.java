b64c76528180971283b257f1229717b52de5a3d4
Simone Joseph <simonelj@byui.edu>
2021-10-08 17:19:34 +0200

    Initial commit

---
+++ src/joseph/HelloMyName.java
@@ -0,0 +1,9 @@
+package joseph;
+
+public class HelloMyName {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Simone.");
+    }
+}
